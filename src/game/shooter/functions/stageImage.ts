9011a33ec42d72e5071176dc38d97ba8d7925596
Sivaa Prasannaa R <31825802+SivaaPrasannaaR@users.noreply.github.com>
2023-02-04 20:18:43 +0530

    shooter image updated

--- src/game/shooter/functions/stageImage.ts
+++ src/game/shooter/functions/stageImage.ts
@@ -3,10 +3,10 @@ import img_1 from "../assets/img_1.png"
 import img_2 from "../assets/img_2.png"
 import img_3 from "../assets/img_3.png"
 import img_4 from "../assets/img_4.png"
-import img_5 from "../assets/img_5.jpg"
-import img_6 from "../assets/img_6.jpg"
-import img_7 from "../assets/img_7.jpg"
-import img_8 from "../assets/img_8.jpg"
+import img_5 from "../assets/img_5.png"
+import img_6 from "../assets/img_6.png"
+import img_7 from "../assets/img_7.png"
+import img_8 from "../assets/img_8.png"
 
 export const stageImage = {
   0: img_0,
